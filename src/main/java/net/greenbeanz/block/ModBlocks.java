483a2d5581de72e4e4e1f3699ed0f1b8672288cf
GREENBEANZ002 <cjpgreen@outlook.com>
2025-07-13 01:30:33 +0100

    Blue dyed oak plank

---
+++ src/main/java/net/greenbeanz/block/ModBlocks.java
@@ -0,0 +1,39 @@
+package net.greenbeanz.block;
+
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.greenbeanz.greenswoods.GreensDyableWoods;
+import net.minecraft.block.AbstractBlock;
+import net.minecraft.block.Block;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.sound.BlockSoundGroup;
+import net.minecraft.util.Identifier;
+
+public class ModBlocks {
+
+    public static final Block BLUE_DYED_OAK_PLANKS = registerBlock("blue_dyed_oak_planks",
+            new Block(AbstractBlock.Settings.create().strength(2.0F, 3.0F).sounds(BlockSoundGroup.WOOD).burnable()));
+
+
+    private static Block registerBlock(String name, Block block) {
+        registerBlockItem(name, block);
+        return Registry.register(Registries.BLOCK, Identifier.of(GreensDyableWoods.MOD_ID, name), block);
+    }
+
+    private static void registerBlockItem(String name, Block block) {
+        Registry.register(Registries.ITEM, Identifier.of(GreensDyableWoods.MOD_ID, name),
+                new BlockItem(block, new Item.Settings()));
+    }
+
+    public static void registerModBlocks() {
+        GreensDyableWoods.LOGGER.info("Registering Mod Blocks for " + GreensDyableWoods.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.BUILDING_BLOCKS).register(entries -> {
+                    entries.add(ModBlocks.BLUE_DYED_OAK_PLANKS);
+                }
+        );
+    }
+}
--- src/main/java/net/greenbeanz/greenswoods/GreensDyableWoods.java
+++ src/main/java/net/greenbeanz/greenswoods/GreensDyableWoods.java
@@ -2,6 +2,7 @@ package net.greenbeanz.greenswoods;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.greenbeanz.block.ModBlocks;
 import net.greenbeanz.greenswoods.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -14,5 +15,6 @@ public class GreensDyableWoods implements ModInitializer {
 	@Override
 	public void onInitialize() {
 		ModItems.registerModItems();
+		ModBlocks.registerModBlocks();
 	}
 }
